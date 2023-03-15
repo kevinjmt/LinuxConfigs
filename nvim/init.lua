df17fb386503fec8f2598c30b5f8bbb6d7707525
Kévin JAMET <keke.jamet@gmail.com>
2023-03-15 23:01:09 +0000

    v0.1
    added close button to KJvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -159,7 +159,7 @@ require('packer').startup(function(use)
             },
             {
               icon='   ',
-              desc='Recent Projects                    ',
+              desc='Recent Projects                 ',
               desc_hl='String',
               key='p',
               action='Telescope projects'
@@ -177,6 +177,13 @@ require('packer').startup(function(use)
               desc_hl='String',
               key='c',
               action='edit ~/.config/nvim/init.lua'
+            },
+            {
+              icon='✘   ',
+              desc='Close KJvim                     ',
+              desc_hl='String',
+              key='q',
+              action=':q'
             },
             -- p = {description = {'  Recent Projects           telescope.builtin.oldfiles()'}, command = 'telescope.builtin.oldfiles()'},
             -- r = {description = {'  Recent Files                  [SPC] [s] [r]'}, command = 'Telescope oldfiles'},
@@ -357,7 +364,8 @@ require('telescope').load_extension('projects')
 pcall(require('telescope').load_extension, 'fzf')
 
 -- See `:help telescope.builtin`
-vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+vim.keymap.set('n', '<leader>rf', require('telescope.builtin').oldfiles, { desc = '[f] Find recently opened files' })
+vim.keymap.set('n', '<leader>rp', require('telescope.builtin').oldfiles, { desc = '[p] Find recently opened projects' })
 vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
 vim.keymap.set('n', '<leader>/', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
@@ -372,6 +380,8 @@ vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+
+vim.keymap.set('n', '<leader>dd', require('telescope.builtin').diagnostics, { desc = 'Show Dashboard' })
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
